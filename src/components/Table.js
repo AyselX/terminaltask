d99a7e3e73ac9c4c689fb570d3aee7cbfe399d19
AyselX <AyselX@users.noreply.github.com>
2020-10-22 09:04:54 +0400

    fist commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,82 +5,60 @@ import React from 'react';
 const Table = () => {
     return (
 
-        <div id= "table">
-            <div className="container">
-            <div className="row">
+        <div id="table">
+            <div id="subtable" className="container-fluid">
+                
 
-                <div className="col-sm-6">
+                    <div id="leftSide">
 
-                    <table>
-                        <tr>
-                            <th>Time:</th>
-                            <td>13:16</td>
-                        </tr>
-
-                        <tr>
-                            <th>Branch:</th>
-                            <td>Inshaatchilar</td>
-                        </tr>
-
-                        <tr>
-                            <th>Service:</th>
-                            <td>Birkat resmileshdirilmesi</td>
-                        </tr>
-
-                        <tr>
-                            <th>Reason:</th>
-                            <td>Call me</td>
-                        </tr>
-
-                        <tr>
-                            <th>No shows:</th>
-                            <td>2</td>
-                        </tr>
-
-                        <tr>
-                            <th>Last Call:</th>
-                            <td>13:30</td>
-                        </tr>
-
-                        <tr>
-                            <th>No show reason:</th>
-                            <td>Zeng chatmadi</td>
-                        </tr>
-
-                        <tr>
-                            <th>Phone number:</th>
-                            <td>+994 50 568 56 55</td>
-                        </tr>
+                        <div className="leftside-1">
+                            <p>Time:</p>
+                            <p>Branch:</p>
+                            <p>Service:</p>
+                            <p>Reason:</p>
+                            <p>No shows:</p>
+                            <p>No shows reason:</p>
+                            <p>Last Call:</p>
+                            <p>Phone Number:</p>
 
 
+                        </div>
+
+                        <div className="leftside-2">
+                            <p>Time:</p>
+                            <p>Branch:</p>
+                            <p>Service:</p>
+                            <p>Reason:</p>
+                            <p>No shows:</p>
+                            <p>No shows reason:</p>
+                            <p>Last Call:</p>
+                            <p>spanhone Number:</p>
+                        </div>
+
+                    </div>
+
+                    <div id="rightSide">
+
+                        <div className="rightSide-1">
+                            <p>Customer name:</p>
+                            <p>Branch:</p>
+                            <p>Comment:</p>
+                        </div>
+
+                        <div className="rightSide-2">
+                            <input></input>
+                            <input></input>
+                            <textarea></textarea>
+                        </div>
 
 
-                    </table>
+                    </div>
 
-                </div>
-
-                <div id="rightSide" className="col-sm-6">
-
-                  <ul>
-                      <li>Customer name:</li>
-                      <li>Branch:</li>
-                      <li>Comment:</li>
-                  </ul>
-                  <ul>
-                      <li><input></input></li>
-                      <li><input></input></li>
-                      <li><textarea></textarea></li>
-                  </ul>
-
-
-                </div>
 
 
             </div>
-
-        </div>
-        </div>
-
+        
+     </div>
     )
 
 }
